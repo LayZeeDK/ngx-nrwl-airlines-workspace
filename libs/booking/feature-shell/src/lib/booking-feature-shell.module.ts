c81abd6ae876722ae0783bbcd761d34899e3ffea
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-04-20 00:30:04 +0200

    feat: register shared data access module

--- libs/booking/feature-shell/src/lib/booking-feature-shell.module.ts
+++ libs/booking/feature-shell/src/lib/booking-feature-shell.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookingDataAccessModule } from '@nrwl-airlines/booking/data-access';
+import { SharedDataAccessModule } from '@nrwl-airlines/shared/data-access';
 
 const routes: Routes = [];
 
@@ -9,6 +10,7 @@ const routes: Routes = [];
   imports: [
     CommonModule,
     RouterModule.forRoot(routes),
+    SharedDataAccessModule,
     BookingDataAccessModule,
   ],
   exports: [RouterModule],
