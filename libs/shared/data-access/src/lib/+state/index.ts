8ae0539c205a16306db042e3f6461fe46fcd7271
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-04-19 23:56:35 +0200

    feat: add root store and store devtools

---
+++ libs/shared/data-access/src/lib/+state/index.ts
@@ -0,0 +1,20 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+
+
+
+export interface AppState {
+
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+
+};
+
+
+export const metaReducers: MetaReducer<AppState>[] = [];
--- libs/shared/data-access/src/lib/shared-data-access.module.ts
+++ libs/shared/data-access/src/lib/shared-data-access.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-
+import { environment } from '../environments/environment';
+import { metaReducers, reducers } from './+state';
 
 @NgModule({
-  declarations: [],
   imports: [
-  ]
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+      },
+    }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
 })
 export class SharedDataAccessModule { }
