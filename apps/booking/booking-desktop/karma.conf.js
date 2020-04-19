ea466bee10a9daad9b6efb5dae276a1b3394efcc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-04-19 03:23:47 +0200

    chore: centralize Karma configuration

--- apps/booking/booking-desktop/karma.conf.js
+++ apps/booking/booking-desktop/karma.conf.js
@@ -1,32 +1,15 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// apps/booking/booking-desktop/karma.conf.js
+const path = require('path');
 
-module.exports = function (config) {
+const getBaseKarmaConfig = require('../../../karma.conf');
+
+module.exports = config => {
+  const baseConfig = getBaseKarmaConfig();
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    ...baseConfig,
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/booking-desktop'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      ...baseConfig.coverageIstanbulReporter,
+      dir: path.join(__dirname, '../../../coverage/apps/booking/booking-desktop'),
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
   });
 };
---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+// /karma.conf.js
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+const { constants } = require('karma');
+const path = require('path');
+
+module.exports = () => ({
+  basePath: '',
+  frameworks: ['jasmine', '@angular-devkit/build-angular'],
+  plugins: [
+    require('karma-jasmine'),
+    require('karma-chrome-launcher'),
+    require('karma-jasmine-html-reporter'),
+    require('karma-coverage-istanbul-reporter'),
+    require('@angular-devkit/build-angular/plugins/karma')
+  ],
+  client: {
+    clearContext: false // leave Jasmine Spec Runner output visible in browser
+  },
+  coverageIstanbulReporter: {
+    dir: path.join(__dirname, 'coverage'),
+    reports: ['html', 'lcovonly', 'text-summary'],
+    fixWebpackSourcePaths: true
+  },
+  reporters: ['progress', 'kjhtml'],
+  port: 9876,
+  colors: true,
+  logLevel: constants.LOG_INFO,
+  autoWatch: true,
+  browsers: ['Chrome'],
+  singleRun: false,
+  restartOnFileChange: true
+});
