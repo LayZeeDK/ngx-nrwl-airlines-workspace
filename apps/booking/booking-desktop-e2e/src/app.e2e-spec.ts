c2de9b0c4ea325be8e90f74d9d22a8bb524aacd5
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-04-28 00:14:29 +0200

    test: fix end-to-end tests

--- apps/booking/booking-desktop-e2e/src/app.e2e-spec.ts
+++ apps/booking/booking-desktop-e2e/src/app.e2e-spec.ts
@@ -1,5 +1,6 @@
-import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
+
+import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,7 +11,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('booking-desktop app is running!');
+    expect(page.getTitleText()).toEqual('booking-desktop');
   });
 
   afterEach(async () => {
--- apps/booking/booking-mobile-e2e/src/app.po.ts
+++ apps/booking/booking-mobile-e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('booking-root .content span')).getText() as Promise<string>;
+    return element(by.css('booking-root h1')).getText() as Promise<string>;
   }
 }
