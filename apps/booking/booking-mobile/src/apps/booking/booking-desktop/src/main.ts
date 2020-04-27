f785f4ac7842cf187a4f2ea3bb15ed36f679f3b7
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-04-27 22:43:41 +0200

    feat: set up entry point for mobile booking application

---
+++ apps/booking/booking-mobile/src/apps/booking/booking-desktop/src/main.ts
@@ -0,0 +1,12 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from '@nrwl-airlines/shared/environments';
+
+import { AppModule } from './app/app.module';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
