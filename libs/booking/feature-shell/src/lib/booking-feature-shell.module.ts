8f1f2b7ebe98c7d3a1771ac930d2c6d634f2f591
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-04-28 23:11:01 +0200

    feat: add shell component to booking feature shell

--- libs/booking/feature-shell/src/lib/booking-feature-shell.module.ts
+++ libs/booking/feature-shell/src/lib/booking-feature-shell.module.ts
@@ -3,25 +3,34 @@ import { RouterModule, Routes } from '@angular/router';
 import { BookingDataAccessModule } from '@nrwl-airlines/booking/data-access';
 import { SharedDataAccessModule } from '@nrwl-airlines/shared/data-access';
 
+import { ShellComponent } from './shell/shell.component';
+
 const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'flight-search',
-  },
-  {
-    path: 'flight-search',
-    loadChildren: () => import('@nrwl-airlines/booking/feature-flight-search')
-      .then(esModule => esModule.BookingFeatureFlightSearchModule),
-  },
-  {
-    path: 'passenger-info',
-    loadChildren: () => import('@nrwl-airlines/booking/feature-passenger-info')
-      .then(esModule => esModule.BookingFeaturePassengerInfoModule),
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'flight-search',
+      },
+      {
+        path: 'flight-search',
+        loadChildren: () => import('@nrwl-airlines/booking/feature-flight-search')
+          .then(esModule => esModule.BookingFeatureFlightSearchModule),
+      },
+      {
+        path: 'passenger-info',
+        loadChildren: () => import('@nrwl-airlines/booking/feature-passenger-info')
+          .then(esModule => esModule.BookingFeaturePassengerInfoModule),
+      },
+    ],
   },
 ];
 
 @NgModule({
+  declarations: [ShellComponent],
   exports: [RouterModule],
   imports: [
     RouterModule.forRoot(routes),
---
+++ libs/booking/feature-shell/src/lib/shell/shell.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'booking-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.css']
+})
+export class ShellComponent { }
