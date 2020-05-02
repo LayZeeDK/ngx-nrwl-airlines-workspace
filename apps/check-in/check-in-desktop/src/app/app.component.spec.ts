8a4990b24b0a7b97a2ef6211622bca5e3567c035
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-05-02 23:14:13 +0200

    feat: generate check-in desktop application

---
+++ apps/check-in/check-in-desktop/src/app/app.component.spec.ts
@@ -0,0 +1,34 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [
+        RouterModule.forRoot([]),
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'check-in-desktop'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('check-in-desktop');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('check-in-desktop');
+  });
+});
---
+++ apps/check-in/check-in-desktop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'check-in-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'check-in-desktop';
+}
